e6c51e80b7a5fb1db7a5f278f34ba565c7667a8b
Dana Dolat <ddolat2@uic.edu>
2017-11-14 06:17:33 +0000

    I did it

---
+++ project_data.js
@@ -0,0 +1,4 @@
+// JavaScript File
+var projects = [
+	{"name": "Weather App", "display": false, "description" : "A progressive web app that utilizes service workers", "technologies": "JavaScript, Service Workers, Material Components", "page": "https://preview.c9users.io/ddolat2/it202/project5/index.html", "repo": "https://github.com/ddolat2/IT202/tree/master/project5"},
+	{"name": "Chicago Food Inspection Search", "display" : true ,"description" : "web app that searched through a database of restaurant inspections and plotted them onto google maps",  "technologies":" ", "page": "https://preview.c9users.io/ddolat2/it202/project3/index.html", "repo": "https://github.com/ddolat2/IT202/tree/master/project3"}
